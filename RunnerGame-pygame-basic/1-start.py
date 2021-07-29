3f97bd0e748b366bfd2f3c029f0ab73db1288ed5
Vivek Mathur <71319149+Grimoors@users.noreply.github.com>
2021-07-29 14:14:45 +0530

    Update 1-start.py
    Added the snail

--- RunnerGame-pygame-basic/1-start.py
+++ RunnerGame-pygame-basic/1-start.py
@@ -2,13 +2,18 @@ import pygame
 import sys as sys
 
 pygame.init()
-screen = pygame.display.set_mode((700,470))
+screen = pygame.display.set_mode((800,470))
 pygame.display.set_caption("Runner UI")
 clock = pygame.time.Clock()
+test_font= pygame.font.Font('font/Pixeltype.ttf', 30)
 
-sky_surface = pygame.image.load('./graphics/Sky.png')
-ground_surface = pygame.image.load ( './graphics/ground.png' )
-#//test_surface.fill('ghostwhite')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load ( 'graphics/ground.png' )
+text_surface= test_font.render("Created by : Grimoors",True,'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos=600
+
 
 while True:
     for event in pygame.event.get():
@@ -18,7 +23,13 @@ while True:
     #draw all elements
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
+    screen.blit(text_surface,(580,430))
 
+    screen.blit(snail_surface,(snail_x_pos,280))
+    snail_x_pos-=4
+    if(snail_x_pos)<=-100: snail_x_pos=800
+    
+        
     #update everything
     pygame.display.update()
     clock.tick ( 60 )
